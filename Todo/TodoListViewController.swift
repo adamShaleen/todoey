5f3dcd383e3ddf381716be1584395efba8e84956
Shaleen, Adam (ETW - FLEX) <Adam.Shaleen@nike.com>
2019-03-18 20:25:50 -0700

    Data source and delegate methods

---
+++ Todo/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Adam Shaleen on 3/18/19.
+//  Copyright © 2019 Adam Shaleen. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let items = ["Thing 1", "Thing 2", "Thing 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let accessoryType = tableView.cellForRow(at: indexPath)?.accessoryType
+        tableView.cellForRow(at: indexPath)?.accessoryType = accessoryType == .checkmark ? .none : .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
